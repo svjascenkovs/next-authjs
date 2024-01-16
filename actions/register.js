4cf8b2e3c660624cac68e48d71e178a3b73b8473
aldis <svjascenkovs@gmail.com>
2024-01-16 12:10:51 +0200

    add create user

--- actions/register.js
+++ actions/register.js
@@ -1,5 +1,8 @@
 "use server";
+import bcrypt from "bcrypt";
 import { RegisterSchema } from "@/schemas";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -8,5 +11,27 @@ export const register = async (values) => {
     return { error: "Invalid fields!" };
   }
 
-  return { success: "Email sent!" };
+  const { email, password, name } = validatedFields.data;
+
+  // Hash password
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  // Check if email is not taken
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email already in use!" };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: Send verification token email
+
+  return { success: "User created!" };
 };
